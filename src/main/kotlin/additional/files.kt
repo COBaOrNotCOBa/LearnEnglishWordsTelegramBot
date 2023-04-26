731f09d2ffe5ea2e8514655e893749086480ff92
COBaOrNotCOBa <yureal1986@mail.ru>
2023-04-26 19:43:49 +0300

    Step_2 26.04.2023 вроде всё так сделал )

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -2,19 +2,33 @@ package additional
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+    var learnedWord: Int,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    val arrayTextFromFile = wordsFile.readLines()
+    var dictionary: Word
 
 //    wordsFile.createNewFile()
 //    wordsFile.writeText("COBa was here")
 //    wordsFile.appendText("Ill be back")
-
 //    println(wordsFile.readLines())
 
-    arrayTextFromFile.forEach{
-        println(it)
+    val listTextFromFile = wordsFile.readLines()
+    listTextFromFile.forEach {
+        val line = it.split("|")
+        dictionary = Word(
+            original = line[0],
+            translate = line[1],
+            correctAnswersCount = line[2].toInt(),
+            learnedWord = line[3].toInt(),
+        )
+        println(dictionary)
     }
 
 }

52fd1257693a70c26e2f17dce6a0e0b50015476a
COBaOrNotCOBa <yureal1986@mail.ru>
2023-05-02 14:22:41 +0300

    Step_5 02.05.2023

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -31,15 +31,15 @@ fun main() {
     var countLearnedWords: Int
     var countAllWords: Int
     var dictionaryNotLearnedWords: List<Word>
-    var dictionaryForUserChoice : List<Word>
-    var userAnswer: Word
+    var dictionaryForUserChoice: List<Word>
+    var rightAnswer: Word
     var userChoice: String = ""
     do {
         println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
         startMenu = readln()
         when (startMenu) {
             "1" -> {
-                while (userChoice != "0") {
+                while (true) {
                     dictionaryNotLearnedWords = dictionary.filter {
                         it.correctAnswersCount < 3
                     }
@@ -48,15 +48,33 @@ fun main() {
                         break
                     }
 
+                    println()
                     println("Выберете вариант ответа от 1 до 4. Или нажмите 0 для возврата в главное меню.")
                     dictionaryForUserChoice = dictionaryNotLearnedWords.shuffled().take(4)
                     for (i in 0..3) {
                         println("${i + 1}: ${dictionaryForUserChoice[i].original}")
                     }
-                    userAnswer = dictionaryNotLearnedWords.shuffled().take(1)[0]
-                    println("Найди перевод слова: ${userAnswer.translate}")
+                    rightAnswer = dictionaryForUserChoice.shuffled().take(1)[0]
+                    println("Найди перевод слова: ${rightAnswer.translate}")
 
                     userChoice = readln()
+
+                    when (userChoice) {
+                        "0" -> break
+                        "1", "2", "3", "4" -> {
+                            if (rightAnswer == dictionaryForUserChoice[userChoice.toInt() - 1]) {
+                                println(
+                                    "Верно! Перевод слова ${rightAnswer.translate} - " +
+                                            dictionaryForUserChoice[userChoice.toInt() - 1].original
+                                )
+                                rightAnswer.correctAnswersCount += 1
+                                saveDictionary(dictionary, wordsFile)
+
+                            } else println("Не верно.")
+                        }
+
+                        else -> println("Не верный номер")
+                    }
                 }
             }
 
@@ -66,11 +84,23 @@ fun main() {
                     it.correctAnswersCount >= 3
                 }.size
 
-                println("Выучено $countLearnedWords из $countAllWords слов | ${100 * countLearnedWords / countAllWords}%")
+                println(
+                    "Выучено $countLearnedWords из $countAllWords слов | " +
+                            "${100 * countLearnedWords / countAllWords}%"
+                )
             }
 
             "0" -> println("Выход")
             else -> println("Введите номер пункта меню")
         }
     } while (startMenu != "0")
+}
+
+fun saveDictionary(dictionary: List<Word>, wordsFile: File) {
+    wordsFile.writeText("")
+    dictionary.forEach {
+        wordsFile.appendText(
+            "${it.original}|${it.translate}|${it.correctAnswersCount}|${it.learnedWord}\n"
+        )
+    }
 }

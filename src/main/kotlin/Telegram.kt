04c73ef6fec11e41205e2d0c5a9ea44c66f55943
COBaOrNotCOBa <yureal1986@mail.ru>
2023-11-29 16:28:39 +0300

    не отправлялись вопросы тем у кого премиум ТГи стоит запрет на получение аудио. Исправил чтобы были хотьбы только вопросы

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -393,10 +393,11 @@ fun sendVoice(botToken: String, chatId: Long, audioFilePath: String): String {
 }
 
 fun sendQuestionAudio(json: Json, botToken: String, chatId: Long, question: Question): String {
-    try {
+    val client = OkHttpClient()
+    return try {
         val sendAudioUrl = "https://api.telegram.org/bot$botToken/sendVoice"
         val fileMediaType = "audio/*".toMediaType()
-        val client = OkHttpClient()
+
         // Отправка аудио файла
         val audioRequestBody = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
@@ -406,41 +407,48 @@ fun sendQuestionAudio(json: Json, botToken: String, chatId: Long, question: Ques
                 File("$AUDIO_PATH${question.correctAnswer.audio}").name,
                 File("$AUDIO_PATH${question.correctAnswer.audio}").asRequestBody(fileMediaType)
             )
-//        .addFormDataPart("duration", "5")
             .build()
+
         val audioRequest = Request.Builder()
             .url(sendAudioUrl)
             .post(audioRequestBody)
             .build()
-        val audioResponse = client.newCall(audioRequest).execute()
-        // Получение URL аудио файла
-        val audioResponseJson = json.decodeFromString<SendAudioResponse>(audioResponse.body?.string() ?: "")
-        val audioUrl = audioResponseJson.result.voice.fileId
-        // Отправка вариантов ответов с аудио
-        val sendMessageUrl = "https://api.telegram.org/bot$botToken/sendMessage"
-        val requestBody = SendMessageRequest(
-            chatId = chatId,
-            text = question.correctAnswer.original,
-            replyMarkup = ReplyMarkup(
-                question.variants.mapIndexed { index, word ->
-                    listOf(
-                        InlineKeyboard(text = word.translate, callbackData = "$CALLBACK_DATA_ANSWER_PREFIX$index")
-                    )
-                }
-            ),
-            audio = audioUrl
-        )
-        val requestBodyString = json.encodeToString(requestBody)
-        val sendMessageRequest = Request.Builder()
-            .url(sendMessageUrl)
-            .header("Content-type", "application/json")
-            .post(requestBodyString.toRequestBody())
-            .build()
-        val sendMessageResponse = client.newCall(sendMessageRequest).execute()
-        return sendMessageResponse.body?.string() ?: ""
 
+        client.newCall(audioRequest).execute().use {
+
+            // Получение URL аудио файла
+//            val audioResponseJson = json.decodeFromString<SendAudioResponse>(audioResponse.body?.string() ?: "")
+//            val audioUrl = audioResponseJson.result.voice.fileId
+
+            // Отправка вариантов ответов с аудио (сейчас без)
+            val sendMessageUrl = "https://api.telegram.org/bot$botToken/sendMessage"
+            val requestBody = SendMessageRequest(
+                chatId = chatId,
+                text = question.correctAnswer.original,
+                replyMarkup = ReplyMarkup(
+                    question.variants.mapIndexed { index, word ->
+                        listOf(
+                            InlineKeyboard(text = word.translate, callbackData = "$CALLBACK_DATA_ANSWER_PREFIX$index")
+                        )
+                    }
+                ),
+//                audio = audioUrl
+            )
+            val requestBodyString = json.encodeToString(requestBody)
+            val sendMessageRequest = Request.Builder()
+                .url(sendMessageUrl)
+                .header("Content-type", "application/json")
+                .post(requestBodyString.toRequestBody())
+                .build()
+
+            client.newCall(sendMessageRequest).execute().use { sendMessageResponse ->
+                sendMessageResponse.body?.string() ?: ""
+            }
+        }
     } catch (e: Exception) {
-        return sendMenu(json, botToken, chatId)
+        // обработка исключения
+        e.printStackTrace()
+        sendMenu(json, botToken, chatId)
     }
 }
 
